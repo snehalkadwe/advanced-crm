3e4c130781c92bd5744bcc0b79b7c9f07c09f128
snehalkadwe <snehalkadwe@gmail.com>
2024-11-29 00:34:38 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\CustomerController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::middleware(['auth', 'role:Admin'])->group(function () {
+    Route::resource('customers', CustomerController::class);
+    Route::resource('sales', SalesController::class);
+});
+Route::middleware(['auth', 'role:Sales Manager'])->group(function () {
+    Route::resource('sales', SalesController::class)->only(['index', 'create', 'store']);
 });
