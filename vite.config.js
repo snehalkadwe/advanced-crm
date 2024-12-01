13fd9f8054283ef72e073a89080a641310dc3fc7
snehalkadwe <snehalkadwe@gmail.com>
2024-12-01 13:41:37 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    build: {
+        manifest: true, // Generates manifest.json
+        outDir: "dist",
+        rollupOptions: {
+            output: {
+                assetFileNames: "[name].[hash].[ext]",
+                chunkFileNames: "[name].[hash].js",
+            },
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
