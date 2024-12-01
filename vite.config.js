448cef69d7917040ad977e18609e7968834b1d70
snehalkadwe <snehalkadwe@gmail.com>
2024-12-01 14:31:59 +0530

    chages in vite file

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    build: {
-        manifest: true, // Generates manifest.json
-        outDir: "dist",
-    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: "public/build",
+        manifest: true,
+        rollupOptions: {
+            input: {
+                main: "resources/js/app.js",
+            },
+        },
+    },
 });
